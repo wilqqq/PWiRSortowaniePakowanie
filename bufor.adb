e139ddc89214834389f00da308f730b621c4d178
Antonia Pękala <antoniap@interia.pl>
2021-01-18 14:46:36 +0100

    komentarze almost done

--- bufor.adb
+++ bufor.adb
@@ -4,31 +4,32 @@ with Ada.Text_IO;
 use Ada.Text_IO;
 
 package body bufor is
-    -- testowy
     protected body BuforP is
 
-        entry Wstaw (X : in TypElementu)--; IleWBuforze : out Integer)
+        entry Wstaw (X : in TypElementu) --wejście do wstawiania elementow danego typu do bufora
            when (Ile < N)
         is -- jesli bufor nie jest pelny to rozpocznij wstawianie, jeśli jest to poczekaj
         begin
-            Bufor (Integer (Ostatni)) := X; --wstawienie wartosci do bufora
-            Ostatni := Ostatni + 1; --nowy indeks wierzchu stosu
+            Bufor (Integer (Ostatni)) := X; --wstawienie wartosci do bufora na indeks "Ostatni"
+            Ostatni := Ostatni + 1; --uaktualnienie indeksu dla kolejnej wstawianej wartości
         end Wstaw;
 
         entry Pobierz (X : out TypElementu) when (Ile > 0)
         is  -- jesli bufor nie jest pusty to rozpocznij pobieranie, jeśli jest to poczekaj
         begin
-            X := Bufor (Integer (Pierwszy)); -- pobranie wartości z wierzchu stosu
-            Pierwszy := Pierwszy + 1; -- uaktualnienie indeksu wierzchu stosu
+            X := Bufor (Integer (Pierwszy)); -- pobranie wartości z indeksu "Pierwszy"
+            Pierwszy := Pierwszy + 1; -- uaktualnienie indeksu kolejnego elementu do pobrania
         end Pobierz;
-        -- podmiana elementu jak w mechaniźmie karuzelowym przez co można pakować elementy innego typu bez potrzeby czekania
+        -- podmiana elementu 
+        --jak w mechaniźmie karuzelowym(w przypadku przepełnionego bufora Pakera 
+        --tego elementu) przez co można pakować elementy innego typu bez potrzeby czekania
         entry Podmien (X : in out TypElementu; sukces : out Boolean)
-            when (Ile > 1) is
+            when (Ile > 0) is -- jesli bufor nie jest pusty to rozpocznij pobieranie, jeśli jest to poczekaj
             indeks : indexerType := Pierwszy;
             tmp : TypElementu;
         begin
             sukces := False;
-            --for indeks in Pierwszy .. Ostatni loop
+            --przeglądnij cały bufor w poszukiwaniu elementu innego typu, jeśli uda się podmienić ustaw zwracany sukces na True
             while indeks /= Ostatni loop
                 if Bufor (Integer (indeks)) /= X then
                     tmp                      := X;
@@ -41,13 +42,13 @@ package body bufor is
             end loop;
         end Podmien;
 
-        -- pokaż zawartość bufora
-        function Poka return String is
+        -- pokaż aktualną zawartość bufora
+        function Wyswietl return String is
             indeks : indexerType := Pierwszy;
             Temp_Unbounded_String : Unbounded_String;
         begin
             Append(Temp_Unbounded_String, " [");
-            while indeks /= Ostatni loop
+            while indeks /= Ostatni loop -- dodaj wszystkie elementy bufora do naszego Stringa
                 Append(Temp_Unbounded_String, ' ');
                 Append(Temp_Unbounded_String, Obraz(Bufor(Integer (indeks))));
                 indeks := indeks + 1;
@@ -56,8 +57,12 @@ package body bufor is
             return To_String(Temp_Unbounded_String);
         end;
 
+        --pomocnicze funkcje zwracające informacje na temat bufora:
+
+        --ilość elementów w buforze
         function Ile return Integer is (Integer (Ostatni - Pierwszy));
 
+        --czy bufor pusty lub pełny
         function Pusty return Boolean is (Ile = 0);
 
         function Pelny return Boolean is (Ile = N - 1);
--- bufor.ads
+++ bufor.ads
@@ -1,22 +1,21 @@
 generic 
-    --N: Natural; --liczba typu integer w przedziale 0..Ostatnia_wartość_integer 
-    type TypElementu is private; -- dowolny typ 
-	with function Obraz (X : TypElementu) return String;
+    type TypElementu is private; -- typ elementów, dla których będzie bufor
+	with function Obraz (X : TypElementu) return String; --pomocnicza funkcja do tworzenia Stringa aby wyświetlić zawartość bufora 
 package bufor is
-    N: constant Natural := 5; -- rozmiar bufora
+    N: constant Natural := 3; -- rozmiar bufora
 	type TypBufora is array(Integer range 0..(N-1)) of TypElementu; --tworzymy bufor(tablicę) długości N i zdefiniowanym wyżej typie elementów
-	type indexerType is mod N; -- indeksery są typu modulo, żeby przeładowanie powodowało przeskok na początek kolejki
+	type indexerType is mod N; -- indeksery są typu modulo, potrzebne aby przeładowanie powodowało przeskok na początek kolejki
 	protected type BuforP is
-		entry Wstaw(X: in TypElementu);--; IleWBuforze: out Integer);
+		entry Wstaw(X: in TypElementu);
 		entry Pobierz(X: out TypElementu);
 		entry Podmien(X : in out TypElementu; sukces : out Boolean);
-		function Poka return String;
+		function Wyswietl return String;
 		function Ile return Integer;
 		function Pusty return Boolean;
 		function Pelny return Boolean;
 	private 
-		Bufor: TypBufora; -- nasz bufor - tablica(realizująca funkcję kolejki)
-		Pierwszy: indexerType := 0;
-		Ostatni: indexerType := 0;
+		Bufor: TypBufora; --nasz bufor - tablica(realizująca funkcję kolejki)
+		Pierwszy: indexerType := 0; --indeks pierwszego elementu w kolejce
+		Ostatni: indexerType := 0; --indeks ostatniego elementu w kolejce
 	end BuforP;
 end bufor;
